32ee7acaac7b559a383995fb43e478552c1a5e40
Kakaiii21 <149568758+Kakaiii21@users.noreply.github.com>
2025-09-03 21:41:00 +0800

    fix the user delete

--- crud_admin/delete.php
+++ crud_admin/delete.php
@@ -5,8 +5,10 @@ session_start();
 if (isset($_GET['deleteid'])) {
     $user_id = intval($_GET['deleteid']);
 
+    // Delete likes of the user first
+    mysqli_query($con, "DELETE FROM likes WHERE user_id = $user_id");
 
-    // Delete posts of the user first
+    // Delete posts of the user
     mysqli_query($con, "DELETE FROM posts WHERE user_id = $user_id");
 
     // Now delete the user
